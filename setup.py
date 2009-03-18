9a25ea2c4d7e3c6187795e414b4f7d017426d295
marcusw <marcusw@cox.net>
2009-03-18 00:46:28 +0000

    Fixed a typo in setup.py.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
 	author_email='dplau@comcast.net, marcusw@cox.net',
 	description='LEGO Mindstorms NXT Control Package',
         url='http://home.comcast.net/~dplau/nxt_python/, http://code.google.com/p/nxt-python/',
-        license='Gnu GPL v2'
+        license='Gnu GPL v2',
 	packages=['nxt'],
 	scripts=['scripts/nxt_push', 'scripts/nxt_test', 'scripts/nxt_filer']
 )
