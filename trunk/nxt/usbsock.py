40616a265b3236808abc9d0e994326ad5afe4858
marcusw@cox.net <marcusw@cox.net@40e99d30-ef16-11dd-9d1d-173cc1d06e3c>
2009-03-28 21:26:31 +0000

    Switched to the GPL v3 or later, and some build changes.

--- trunk/nxt/usbsock.py
+++ trunk/nxt/usbsock.py
@@ -2,9 +2,9 @@
 # Copyright (C) 2006-2007  Douglas P Lau
 # Copyright (C) 2009  Marcus Wanner
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
