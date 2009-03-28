880e865e92847deb80558da68316aca66e4daa52
marcusw <marcusw@cox.net>
2009-03-28 21:26:31 +0000

    Switched to the GPL v3 or later, and some build changes.

--- nxt/server.py
+++ nxt/server.py
@@ -1,8 +1,9 @@
+# nxt.server module -- LEGO Mindstorms NXT socket interface module
 # Copyright (C) 2009  Marcus Wanner
 #
-# This program is free software; you can redistribute it and/or modify
+# This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
+# the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@
 
 from distutils.core import setup
 
+readme = open('Readme.txt', 'r')
+ldesc = readme.read(8192)
+readme.close()
+
 setup(
 	name='NXT-Python',
-	version='0.8',
+	version='0.8.1',
 	author='Douglas Lau, Marcus Wanner',
 	author_email='dplau@comcast.net, marcusw@cox.net',
 	description='LEGO Mindstorms NXT Control Package',
         url='http://home.comcast.net/~dplau/nxt_python/, http://code.google.com/p/nxt-python/',
         license='Gnu GPL v2',
 	packages=['nxt'],
-	scripts=['scripts/nxt_push', 'scripts/nxt_test', 'scripts/nxt_filer']
+	scripts=['scripts/nxt_push', 'scripts/nxt_test', 'scripts/nxt_filer'],
+        long_description=ldesc
 )
