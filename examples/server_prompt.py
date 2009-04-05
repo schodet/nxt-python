405d2ca0e1023feac5b18af3023f21662ff49da7
marcusw <marcusw@cox.net>
2009-04-05 01:14:10 +0000

    nxt.server improvements, and changed it back from localhost to any message sender, as it has to be sent specifically to the server's ip address. Might add optional password protection later.

--- examples/server_prompt.py
+++ examples/server_prompt.py
@@ -1,17 +1,26 @@
 #This script is an example for the nxt.server module. You need to run
-#nxt.server.serve_forever() in another window.
+#nxt.server.serve_forever() in another window. Or, if you want to use
+#this across a network, pass the IP of the computer running the server
+#as an argument in the command line.
 
-import socket
+import socket, sys
+
+try:
+    server = sys.argv[1]
+    bindto = ''
+except:
+    server = 'localhost'
+    bindto = 'localhost'
 
 insock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 outsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-insock.bind(('localhost', 54374))
+insock.bind((bindto, 54374))
 
 while 1:
     command = raw_input('nxt> ')
-    outsock.sendto(command, ('localhost', 54174))
+    outsock.sendto(command, (server, 54174))
     retvals, addr = insock.recvfrom(1024)
     retcode = retvals[0]
-    retmsg = retvals[1:retvals.index('~')]
+    retmsg = retvals[1:len(retvals)]
     print 'Return code: '+retcode
     print 'Return message: '+retmsg
