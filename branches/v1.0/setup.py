77cdaf5717f69e29d6647e00c429d62e845ba12b
marcusw@cox.net <marcusw@cox.net@40e99d30-ef16-11dd-9d1d-173cc1d06e3c>
2009-07-24 15:06:27 +0000

    Actually version 1.0.1...

--- branches/v1.0/setup.py
+++ branches/v1.0/setup.py
@@ -8,7 +8,7 @@ readme.close()
 
 setup(
 	name='NXT-Python',
-	version='1.1',
+	version='1.0.1',
 	author='Douglas Lau, Marcus Wanner',
 	author_email='dplau@comcast.net, marcusw@cox.net',
 	description='LEGO Mindstorms NXT Control Package',
