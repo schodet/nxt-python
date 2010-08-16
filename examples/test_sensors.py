0bb88e8576d2f260d720106b190d1c5ff7543736
marcusw <marcusw@cox.net>
2010-08-16 19:22:12 +0000

    Added a tag for v2.0-beta-1 from the v2 branch.

--- examples/test_sensors.py
+++ examples/test_sensors.py
@@ -3,19 +3,9 @@
 import nxt.locator
 from nxt.sensor import *
 
-def test_sensors(b):
-	print 'Touch:',
-	if TouchSensor(b, PORT_1).get_sample():
-		print 'yes'
-	else:
-		print 'no'
-	print 'Sound:', SoundSensor(b, PORT_2).get_sample()
-	print 'Light:', LightSensor(b, PORT_3).get_sample()
-	print 'Ultrasonic:', UltrasonicSensor(b, PORT_4).get_sample()
+b = nxt.locator.find_one_brick()
 
-sock = nxt.locator.find_one_brick()
-if sock:
-	test_sensors(sock.connect())
-	sock.close()
-else:
-	print 'No NXT bricks found'
+print 'Touch:', Touch(b, PORT_1).get_sample()
+print 'Sound:', Sound(b, PORT_2).get_sample()
+print 'Light:', Light(b, PORT_3).get_sample()
+print 'Ultrasonic:', Ultrasonic(b, PORT_4).get_sample()
--- nxt/telegram.py
+++ nxt/telegram.py
@@ -19,97 +19,97 @@ from struct import pack, unpack
 import nxt.error
 
 class InvalidReplyError(Exception):
-	pass
+    pass
 
 class InvalidOpcodeError(Exception):
-	pass
+    pass
 
 class Telegram(object):
 
-	TYPE = 0	# type byte offset
-	CODE = 1	# code byte offset
-	DATA = 2	# data byte offset
+    TYPE = 0    # type byte offset
+    CODE = 1    # code byte offset
+    DATA = 2    # data byte offset
 
-	TYPE_NOT_DIRECT = 0x01		# system vs. direct type
-	TYPE_REPLY = 0x02		# reply type (from NXT brick)
-	TYPE_REPLY_NOT_REQUIRED = 0x80	# reply not required flag
+    TYPE_NOT_DIRECT = 0x01          # system vs. direct type
+    TYPE_REPLY = 0x02               # reply type (from NXT brick)
+    TYPE_REPLY_NOT_REQUIRED = 0x80  # reply not required flag
 
-	def __init__(self, direct=False, opcode=0, reply_req=True, pkt=None):
-		if pkt:
-			self.pkt = StringIO(pkt)
-			self.typ = self.parse_u8()
-			self.opcode = self.parse_u8()
-			if not self.is_reply():
-				raise InvalidReplyError
-			if self.opcode != opcode:
-				raise InvalidOpcodeError, self.opcode
-		else:
-			self.pkt = StringIO()
-			typ = 0
-			if not direct:
-				typ |= Telegram.TYPE_NOT_DIRECT
-			if not reply_req:
-				typ |= Telegram.TYPE_REPLY_NOT_REQUIRED
-			self.add_u8(typ)
-			self.add_u8(opcode)
+    def __init__(self, direct=False, opcode=0, reply_req=True, pkt=None):
+        if pkt:
+            self.pkt = StringIO(pkt)
+            self.typ = self.parse_u8()
+            self.opcode = self.parse_u8()
+            if not self.is_reply():
+                raise InvalidReplyError
+            if self.opcode != opcode:
+                raise InvalidOpcodeError, self.opcode
+        else:
+            self.pkt = StringIO()
+            typ = 0
+            if not direct:
+                typ |= Telegram.TYPE_NOT_DIRECT
+            if not reply_req:
+                typ |= Telegram.TYPE_REPLY_NOT_REQUIRED
+            self.add_u8(typ)
+            self.add_u8(opcode)
 
-	def __str__(self):
-		return self.pkt.getvalue()
+    def __str__(self):
+        return self.pkt.getvalue()
 
-	def is_reply(self):
-		return self.typ == Telegram.TYPE_REPLY
+    def is_reply(self):
+        return self.typ == Telegram.TYPE_REPLY
 
-	def add_string(self, n_bytes, v):
-		self.pkt.write(pack('%ds' % n_bytes, v))
+    def add_string(self, n_bytes, v):
+        self.pkt.write(pack('%ds' % n_bytes, v))
 
-	def add_filename(self, fname):
-		self.pkt.write(pack('20s', fname))
+    def add_filename(self, fname):
+        self.pkt.write(pack('20s', fname))
 
-	def add_s8(self, v):
-		self.pkt.write(pack('<b', v))
+    def add_s8(self, v):
+        self.pkt.write(pack('<b', v))
 
-	def add_u8(self, v):
-		self.pkt.write(pack('<B', v))
+    def add_u8(self, v):
+        self.pkt.write(pack('<B', v))
 
-	def add_s16(self, v):
-		self.pkt.write(pack('<h', v))
+    def add_s16(self, v):
+        self.pkt.write(pack('<h', v))
 
-	def add_u16(self, v):
-		self.pkt.write(pack('<H', v))
+    def add_u16(self, v):
+        self.pkt.write(pack('<H', v))
 
-	def add_s32(self, v):
-		self.pkt.write(pack('<i', v))
+    def add_s32(self, v):
+        self.pkt.write(pack('<i', v))
 
-	def add_u32(self, v):
-		self.pkt.write(pack('<I', v))
+    def add_u32(self, v):
+        self.pkt.write(pack('<I', v))
 
-	def parse_string(self, n_bytes=0):
-		if n_bytes:
-			return unpack('%ss' % n_bytes,
-				self.pkt.read(n_bytes))[0]
-		else:
-			return self.pkt.read()
+    def parse_string(self, n_bytes=0):
+        if n_bytes:
+            return unpack('%ss' % n_bytes,
+                self.pkt.read(n_bytes))[0]
+        else:
+            return self.pkt.read()
 
-	def parse_s8(self):
-		return unpack('<b', self.pkt.read(1))[0]
+    def parse_s8(self):
+        return unpack('<b', self.pkt.read(1))[0]
 
-	def parse_u8(self):
-		return unpack('<B', self.pkt.read(1))[0]
+    def parse_u8(self):
+        return unpack('<B', self.pkt.read(1))[0]
 
-	def parse_s16(self):
-		return unpack('<h', self.pkt.read(2))[0]
+    def parse_s16(self):
+        return unpack('<h', self.pkt.read(2))[0]
 
-	def parse_u16(self):
-		return unpack('<H', self.pkt.read(2))[0]
+    def parse_u16(self):
+        return unpack('<H', self.pkt.read(2))[0]
 
-	def parse_s32(self):
-		return unpack('<i', self.pkt.read(4))[0]
+    def parse_s32(self):
+        return unpack('<i', self.pkt.read(4))[0]
 
-	def parse_u32(self):
-		return unpack('<I', self.pkt.read(4))[0]
+    def parse_u32(self):
+        return unpack('<I', self.pkt.read(4))[0]
 
-	def check_status(self):
-		nxt.error.check_status(self.parse_u8())
+    def check_status(self):
+        nxt.error.check_status(self.parse_u8())
 
 import nxt.direct
 import nxt.system
