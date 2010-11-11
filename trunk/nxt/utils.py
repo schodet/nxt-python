e35d3630eee6179dff255caad7fa6b695c47d935
marcusw@cox.net <marcusw@cox.net@40e99d30-ef16-11dd-9d1d-173cc1d06e3c>
2010-11-11 17:48:11 +0000

    Added proper GPL copyright declaration to the new utils.py.

--- trunk/nxt/utils.py
+++ trunk/nxt/utils.py
@@ -1,6 +1,15 @@
-'''
-Various utils
-'''
+# nxt.utils module -- Generic utilities to support other modules
+# Copyright (C) 2010  Vladimir Moskva
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 from collections import defaultdict
 
@@ -21,3 +30,4 @@ def parse_command_line_arguments(arguments):
             else:
                 parameters.append(argument)
     return parameters, keyword_parameters
+
