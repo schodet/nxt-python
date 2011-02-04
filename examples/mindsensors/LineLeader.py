7acccdeded3c5dd0600997e19f79b0510dcc4ef7
marcusw <marcusw@cox.net>
2011-02-04 00:29:23 +0000

    Fixing one of the mindsensors examples.

--- examples/mindsensors/LineLeader.py
+++ examples/mindsensors/LineLeader.py
@@ -7,10 +7,7 @@ from nxt.sensor import *
 b = nxt.locator.find_one_brick()#find brick and connect
 s = MSLineLeader(b, PORT_1)     #which sensor, brick and port
 
-s.command(c.Commands.SENSOR_WAKE)          #sending commands
-s.set_pid('i', 25)          # p, i, or d depending on which you want
-s.set_pid_divisor('i', 32)  #same as above
-s.set_set_point(50)
+s.command(s.Commands.SENSOR_WAKE)          #sending commands
 print s.get_steering()
 print 'Result: ' + str(bin(s.get_result())) #to print result as byte
 print 'Average: ' + str(s.get_average())    #Inserting text before values
