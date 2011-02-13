a1791233c7693adc5b20caf082e019c8bd3b6267
marcusw@cox.net <marcusw@cox.net@40e99d30-ef16-11dd-9d1d-173cc1d06e3c>
2011-02-13 20:52:42 +0000

    Minor changes to install and build system.

--- trunk/install.bat
+++ trunk/install.bat
@@ -4,8 +4,8 @@ if errorlevel 1 goto error
 goto end
 :error
 echo -=-=-=-=-=-=-=-=-=-=-
-echo There was an error during the installation. Please check the above text
-echo for more information before you
+echo There was an error during the installation.
+echo Please check the above text for more information.
 pause
 :end
 @echo on
