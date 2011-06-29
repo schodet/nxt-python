b433968225299e87f73a753494ff42ea245531ad
marcus <marcus@wanners.net>
2011-06-29 00:44:52 +0000

    Added preliminary support for file configuration of nxt location information.

--- nxt/locator.py
+++ nxt/locator.py
@@ -12,7 +12,7 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
-import traceback
+import traceback, ConfigParser, os
 
 class BrickNotFoundError(Exception):
     pass
@@ -27,8 +27,7 @@ find_one_brick. Any or all can be selected."""
         #new method options MUST default to False!
         self.usb = usb
         self.bluetooth = bluetooth
-        if fantomusb or fantombt:
-            self.fantom = True
+        self.fantom = fantomusb or fantombt
         self.fantomusb = fantomusb
         self.fantombt = fantombt
 
@@ -81,21 +80,33 @@ def find_bricks(host=None, name=None, silent=False, method=Method()):
         raise NoBackendError("No selected backends are available! Did you install the comm modules?")
 
 
-def find_one_brick(host=None, name=None, silent=False, strict=True, debug=False, method=Method()):
-    """Use to find one brick. After it returns a usbsock object or a bluesock
-object, it automatically connects to it. The host and name args limit
-the search to a given MAC or brick name. Set silent to True to stop
-nxt-python from printing anything during the search. This function by default 
+def find_one_brick(host=None, name=None, silent=False, strict=None, debug=False, method=None, confpath=None):
+    """Use to find one brick. The host and name args limit the search to 
+a given MAC or brick name. Set silent to True to stop nxt-python from 
+printing anything during the search. This function by default 
 automatically checks to see if the brick found has the correct host/name 
-(if either are provided) and will not return a brick which doesn't
-match. This can be disabled (so the function returns any brick which
-can be connected to and provides a valid reply to get_device_info()) by
-passing strict=False. This will, however, still tell the USB/BT backends to 
-only look for devices which match the args provided."""
+(if either are provided) and will not return a brick which doesn't 
+match. This can be disabled (so the function returns any brick which can 
+be connected to and provides a valid reply to get_device_info()) by 
+passing strict=False. This will, however, still tell the comm backends 
+to only look for devices which match the args provided. The confpath arg 
+specifies the location of the configuration file which brick location 
+information will be read from if no brick location directives (host, 
+name, strict, or method) are provided."""
     if debug and silent:
         silent=False
         print "silent and debug can't both be set; giving debug priority"
 
+    conf = read_config(confpath, debug)
+    if not (host or name or strict or method):
+        host	= conf.get('Brick', 'host')
+        name	= conf.get('Brick', 'name')
+        strict	= conf.get('Brick', 'strict')
+        method	= eval('Method(%s)' % conf.get('Brick', 'method'))
+    if not strict: strict = True
+    if not method: method = Method()
+    print host, name, strict, method
+    
     for s in find_bricks(host, name, silent, method):
         try:
             if host and 'host' in dir(s) and s.host != host:
@@ -128,3 +139,13 @@ def server_brick(host, port = 2727):
     import ipsock
     sock = ipsock.IpSock(host, port)
     return sock.connect()
+
+
+def read_config(confpath=None, debug=False):
+    conf = ConfigParser.RawConfigParser({'host': None, 'name': None, 'strict': True, 'method': ''})
+    if not confpath: confpath = os.path.expanduser('~/.nxt-python')
+    if conf.read([confpath]) == [] and debug:
+        print "Warning: Config file (should be at %s) was not read" % confpath
+    if conf.has_section('Brick') == False:
+        conf.add_section('Brick')
+    return conf
