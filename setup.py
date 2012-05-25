2ce6784e9a985373d3ff63cbc26fc0bf03ece592
marcus <marcus@wanners.net>
2012-05-25 23:26:12 +0000

    v2.2.2: Tag from v2.2 branch r380

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ except:
 
 setup(
     name='nxt-python',
-    version='2.2',
+    version='2.2.2',
     author='Marcus Wanner',
     author_email='marcus@wanners.net',
     description='LEGO Mindstorms NXT Control Package',
