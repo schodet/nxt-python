cd19dfcb2eece28dc1ab8b4a1bacaebb1fa56a83
marcus@wanners.net <marcus@wanners.net@40e99d30-ef16-11dd-9d1d-173cc1d06e3c>
2012-08-06 22:01:27 +0000

    Fixing Issue 43 problem in MSACCL example as pointed out by christophegragnic.

--- trunk/examples/mindsensors/ACCL.py
+++ trunk/examples/mindsensors/ACCL.py
@@ -10,7 +10,7 @@ print s.get_range('x')              #range (xyz)
 s.command(s.Commands.X_CALIBRATION) #See user guide or class def for more info
 for i in range(10):
     print 'Tilt: ' + str(s.get_all_tilt())        #all tilt data
-    print 'Tilt Y: ' + str(s.get_tilt('x'))     #x, y, or z get tilt data (works for accel too)
+    print 'Tilt Y: ' + str(s.get_tilt('y'))     #x, y, or z get tilt data (works for accel too)
     print 'Accel: ' + str(s.get_sample())       #all accel data
     time.sleep(0.2)
 b.sock.close()  #close bluetooth connection
