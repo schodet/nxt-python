0f9ae43696c092538b64d29cadd08d41fe7eb528
Eelviny <elvinluff@gmail.com>
2016-04-22 21:44:44 +0100

    Ensure the script affects any and only Python 3.x releases

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
-#!/bin/bash -e
+#!/bin/bash
 
 echo "This builds nxt-python for Python 3"
 echo "and installs it to the user's home directory."
 echo "Root is not required."
 echo ""
 
-rm -Rf ~/.local/lib/python3.4/site-packages/nxt
-rm -Rf ~/.local/lib/python2.7/site-packages/nxt # should not be needed
+rm -Rf ~/.local/lib/python3.*/site-packages/nxt
 python3 setup.py install --user
