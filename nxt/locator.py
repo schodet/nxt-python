schodet/nxt-python

commit cad30b1a6e02914c3e69cb6a017a052e334bce78

don't use eval() for parsing method in config file
This improves the config file parsing of the method property. It fixes
possible code injection via eval() as well as makes parsing a bit more
robust by filtering out invalid values, such as the obsolete fantom
driver values.

Fixes #137

nxt/locator.py
@@ -99,7 +99,10 @@ name, strict, or method) are provided."""
         host	= conf.get('Brick', 'host')
         name	= conf.get('Brick', 'name')
         strict	= bool(int(conf.get('Brick', 'strict')))
-        method	= eval('Method(%s)' % conf.get('Brick', 'method'))
+        methods = map(lambda x: x.strip().split('='),
+                      conf.get('Brick', 'method').split(','))
+        method = Method(**{k: v == 'True' for k, v in methods
+                           if k in ('bluetooth', 'usb', 'device')})
     if not strict: strict = True
     if not method: method = Method()
 

commit a111be0010b8743e7564b7d97c3242b66b3b174a

fix crash when no config file
This fixes an exception when there is not config file. The changes from commit cad30b1a6e02914c3e69cb6a017a052e334bce78 introduced this error.

nxt/locator.py
@@ -99,10 +99,12 @@ name, strict, or method) are provided."""
         host	= conf.get('Brick', 'host')
         name	= conf.get('Brick', 'name')
         strict	= bool(int(conf.get('Brick', 'strict')))
-        methods = map(lambda x: x.strip().split('='),
-                      conf.get('Brick', 'method').split(','))
-        method = Method(**{k: v == 'True' for k, v in methods
-                           if k in ('bluetooth', 'usb', 'device')})
+        method_value = conf.get('Brick', 'method')
+        if method_value:
+            methods = map(lambda x: x.strip().split('='),
+                          method_value.split(','))
+            method = Method(**{k: v == 'True' for k, v in methods
+                               if k in ('bluetooth', 'usb', 'device')})
     if not strict: strict = True
     if not method: method = Method()
 