863223fe815ff0d6809472e36b147ec525535138
Nicolas Schodet <nico@ni.fr.eu.org>
2021-10-26 00:06:56 +0200

    Update packaging

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "This builds nxt-python for Python 3"
-echo "and installs it to the user's home directory."
-echo "Root is not required."
-echo ""
-
-rm -Rf ~/.local/lib/python3.*/site-packages/nxt
-python3 setup.py install --user
--- setup.py
+++ setup.py
@@ -1,23 +1,3 @@
-#!/usr/bin/env python
+import setuptools
 
-from distutils.core import setup
-
-try:
-    readme = open('README', 'r')
-    ldesc = readme.read(20000)
-    readme.close()
-except:
-    ldesc = ""
-
-setup(
-    name='nxt-python',
-    version='3.0',
-    author='Elvin Luff',
-    author_email='elvinluff@gmail.com',
-    description='LEGO Mindstorms NXT Control Package',
-    url='https://github.com/Eelviny/nxt-python/',
-    license='Gnu GPL v3',
-    packages=['nxt', 'nxt.sensor'],
-    scripts=['scripts/nxt_push', 'scripts/nxt_test', 'scripts/nxt_server', 'scripts/nxt_filer'],
-    long_description=ldesc
-)
+setuptools.setup()
