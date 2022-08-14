b2d3e6af3fbe079dab205671dbc8bd9a3d515c80
Jared Dunbar <jrddunbr@ja4.org>
2022-08-14 13:17:19 -0400

    Adds 'experiments' from manual plus fixed driver
    Turns out that the Strobe output does 'work', and
    I also identified some issues with the analog
    output modes that was causing frequencies to be
    byte-swapped. Still more testing/work to be done.

---
+++ examples/hitechnic-superpro/example7.py
@@ -0,0 +1,36 @@
+import nxt.locator
+from nxt.sensor.hitechnic import SuperPro
+
+"""
+Experiment 7, adapted from the SuperPro demo
+
+For this demo, connect a LED between B4 and GND. Connect the magnetic hall-effect sensor (A3212EUA-T) as follows:
+
+* Pin 1: 3V
+* Pin 2: GND
+* Pin 3: B0
+
+Connect B0 to 3V with a 10kOhm resistor
+
+Note: My magnetic hall effect sensor appears to be broken, so I couldn't really test this. RIP
+"""
+
+# Find NXT, configure sensor
+brick = nxt.locator.find()
+pro = SuperPro(brick, nxt.sensor.Port.S1)
+
+# Configure B4 as output
+pro.set_digital_modes_byte(0b00010000)
+pro.set_digital_byte(0x00)
+
+while True:
+    try:
+        # Read B0 (magnet signal is inverted, low = magnet, then write back B4 with the value.
+        hall_effect_sensor = pro.get_digital()["b0"]
+        print("Magnet: {}".format(not hall_effect_sensor))
+        pro.set_digital_byte((not hall_effect_sensor) << 4)
+    except KeyboardInterrupt:
+        break
+
+# When program stopped, turn off outputs
+pro.set_digital_byte(0x00)
---
+++ examples/hitechnic-superpro/example9.py
@@ -0,0 +1,41 @@
+from time import sleep
+import nxt.locator
+from nxt.sensor.hitechnic import SuperPro
+
+"""
+Experiment 9, adapted from the SuperPro demo
+
+For this demo, connect a speaker between O0 and O1.
+"""
+A4_FREQ = 440
+C4_FREQ = 261
+DS4_FREQ = 311
+E4_FREQ = 329
+F4_FREQ = 349
+G4_FREQ = 392
+
+# Find NXT, configure sensor
+brick = nxt.locator.find()
+pro = SuperPro(brick, nxt.sensor.Port.S1)
+
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, C4_FREQ, 3.3)
+sleep(0.2)
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, DS4_FREQ, 3.3)
+sleep(0.2)
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, E4_FREQ, 3.3)
+sleep(0.2)
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, 1, 0.0)
+
+sleep(0.2)
+
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, F4_FREQ, 3.3)
+pro.analog_out_voltage(1, SuperPro.AnalogOutputMode.SQUARE, A4_FREQ, 3.3)
+sleep(0.2)
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, E4_FREQ, 3.3)
+pro.analog_out_voltage(1, SuperPro.AnalogOutputMode.SQUARE, G4_FREQ, 3.3)
+sleep(0.2)
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, C4_FREQ, 3.3)
+pro.analog_out_voltage(1, SuperPro.AnalogOutputMode.SQUARE, E4_FREQ, 3.3)
+sleep(0.2)
+pro.analog_out_voltage(0, SuperPro.AnalogOutputMode.SQUARE, 1, 0.0)
+pro.analog_out_voltage(1, SuperPro.AnalogOutputMode.SQUARE, 1, 0.0)
